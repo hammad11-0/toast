fe62c0ab958e6fa62da214b45affff4ec2183ce4
hammad11-0 <bitf18a504@pucit.edu.pk>
2021-11-30 20:04:21 +0500

    toast

--- app/src/main/java/com/example/toast/MainActivity.java
+++ app/src/main/java/com/example/toast/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1=findViewById(R.id.button);
+
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Toast In Generated",Toast.LENGTH_LONG).show();
+
+            }
+        });
+
     }
 }
